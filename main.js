5cf7ff1f4644687c123377ae3fa63ddf7d7bf3cf
Davide Lugli <luglidavide.web@gmail.com>
2020-01-17 16:07:39 +0100

    Next Completo ( arrow e pallini)

--- main.js
+++ main.js
@@ -20,14 +20,20 @@ $(document).ready(
 function clickNext() {
   var imgActive = $('img.active');
   var imgNext = imgActive.next();
+  var circleActive = $('i.active');
+  var circleNext = circleActive.next();
   var isLast = imgActive.hasClass('last');
   if (isLast) {
     imgActive.removeClass('active');
     $('img.first').addClass('active');
+    circleActive.removeClass('active');
+    $('i.first').addClass('active');
 
   } else {
     imgActive.removeClass('active');
     imgNext.addClass('active');
+    circleActive.removeClass('active');
+    circleNext.addClass('active');
   }
 
 }
